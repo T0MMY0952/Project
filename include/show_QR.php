a1a52cf210a3b39969748c89a7a82974415989ef
Jeetomz <evo.ix@windowslive.com>
2018-03-21 15:46:12 +0700

    edit tracking and qr code

--- include/show_QR.php
+++ include/show_QR.php
@@ -8,7 +8,7 @@ $qr = new QR_BarCode();
 // create text QR code 
 $id = $_GET['idshipment'];
 $type = $_GET['type'];
-$qr->text("158.108.207.4/sp_60_TrackingForAg/include/tracking.php?idshipment=".$id"&type=".$type); 
+$qr->text("158.108.207.4/sp_60_TrackingForAg/include/tracking.php?idshipment=".$id."&type=".$type); 
 
 // display QR code image
 $qr->qrCode();
