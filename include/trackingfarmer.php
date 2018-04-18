db46edc6bd69155858fed4bfaaf95e37a0813f13
Jeetomz <evo.ix@windowslive.com>
2018-04-18 16:42:12 +0700

    edit exportdate
    - แก้บั๊กวันส่งออกไม่แสดง

--- include/trackingfarmer.php
+++ include/trackingfarmer.php
@@ -29,7 +29,7 @@
   <div class="form-group form-group-sm">
     <label for="inputEmail3" class="col-sm-4 control-label">วันที่ส่งออก</label>
     <div class="col-sm-9">
-      <input type="email" class="form-control" id="inputEmail3" value="<?php echo $getinfo1['ap_exportdate']; ?>" disabled="disabled">
+      <input type="email" class="form-control" id="inputEmail3" value="<?php echo $getinfo1['exportdate']; ?>" disabled="disabled">
     </div>
   </div>
   <div class="form-group form-group-sm">
